d68cecfb6c833c204e76b0f8d71e02081f1c2539
Kjip <kjipgamer@gmail.com>
2019-04-28 13:29:17 +0200

    Added main menu

---
+++ Ludum Dare 44/Assets/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuScript : MonoBehaviour {
+
+    public TextMeshProUGUI highscore;
+
+    private void Start() {
+        highscore.text = "Highscore: " + PlayerPrefs.GetInt("highscore").ToString();
+    }
+
+
+    public void Play() { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+
+}
