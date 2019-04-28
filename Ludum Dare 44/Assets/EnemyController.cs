14dc91823596921697f82d868659211d3f57f053
Kjip <kjipgamer@gmail.com>
2019-04-28 11:30:58 +0200

    Added enemy AI

---
+++ Ludum Dare 44/Assets/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+  
+    Transform player;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        agent.SetDestination(player.position);
+
+        agent.stoppingDistance = 0;
+
+        if(distance <= agent.stoppingDistance)
+        {
+            FaceTarget();
+        }
+    }
+
+    void FaceTarget() {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
