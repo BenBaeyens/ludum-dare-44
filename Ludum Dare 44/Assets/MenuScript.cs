40a6a524039bb4e6d6fa83f7e763a10dc0217b8a
Kjip <kjipgamer@gmail.com>
2019-04-28 17:52:55 +0200

    Added pause menu

--- Ludum Dare 44/Assets/MenuScript.cs
+++ Ludum Dare 44/Assets/MenuScript.cs
@@ -9,6 +9,10 @@ public class MenuScript : MonoBehaviour {
     public AudioClip hoversound;
     public AudioClip clicksound;
 
+    public bool inGame = false;
+    private bool paused = false;
+    public GameObject pauseMenu;
+
     AudioSource audioSource;
 
     public TextMeshProUGUI highscore;
@@ -16,10 +20,26 @@ public class MenuScript : MonoBehaviour {
     private void Start() {
         highscore.text = "Highscore: " + PlayerPrefs.GetInt("highscore").ToString();
         audioSource = gameObject.AddComponent<AudioSource>();
+
     }
 
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && inGame)
+        {
+            Debug.Log("test");
+            if (paused)
+            {
+                UnPauseGame();
+            }
+            if (!paused)
+            {
+                Debug.Log("paused");
+                PauseGame();
+            }
+        }
+    }
 
-    public void Play() { 
+    public void Play() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
@@ -39,7 +59,19 @@ public class MenuScript : MonoBehaviour {
     }
     public void PlayAgain() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 
+    public void PauseGame() {
+        paused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
 
+    public void UnPauseGame() {
+        paused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+    
 }
